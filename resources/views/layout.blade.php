5060018f0027097c9aff0c754769a4a5b27d7d4a
kdalosa <karsheenadalosa@gmail.com>
2025-05-24 22:19:12 +0800

    Add files via upload

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Item Listings</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-4">
+    <h2 class="mb-4">Item Listings</h2>
+    @yield('content')
+</div>
+</body>
+</html>
