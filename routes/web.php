64eacab065040b0c90f0421af8d33c8ed502f1b8
kdalosa <karsheenadalosa@gmail.com>
2025-05-24 22:18:08 +0800

    added route for listings

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
+
+Route::resource('items', ItemController::class);
+Route::post('items/{id}/status', [ItemController::class, 'updateStatus'])->name('items.updateStatus');
 
 Route::get('/', function () {
     return view('welcome');
