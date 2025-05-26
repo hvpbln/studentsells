5c1de1df0d227ec7f36cc1e7e512e4e50b8f8b96
Jella Krishna <jellacoronado08@gmail.com>
2025-05-26 22:20:39 +0800

    repositioned logout in the navigation bar

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -126,12 +126,16 @@
         </div>
 
         <nav>
-        <a href="#">Home</a>
-        <a href="{{ route('items.index') }}">Shop</a>
-        <a href="{{ route('wishlists.index') }}">Wishlist</a>
-        <a href="#">Message</a>
-        <a href="#">Logout</a>
+            <a href="#">Home</a>
+            <a href="{{ route('items.index') }}">Shop</a>
+            <a href="{{ route('wishlists.index') }}">Wishlist</a>
+            <a href="#">Message</a>
+            <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
         </nav>
+
+        <form id="logout-form" method="POST" action="{{ route('logout') }}" style="display: none;">
+            @csrf
+        </form>
 
         <div class="right-section">
         <div class="search-box">
