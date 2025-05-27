5038873182a504070f515e53a4e4d0a79c01a2a8
kdalosa <karsheenadalosa@gmail.com>
2025-05-27 20:37:25 +0800

    fixed able to upload images when initial list doesn't have one

--- resources/views/items/edit.blade.php
+++ resources/views/items/edit.blade.php
@@ -3,11 +3,22 @@
 @section('content')
 <h4>Edit Listing</h4>
 
-<form method="POST" action="{{ route('items.update', $item->id) }}">
-    @csrf @method('PUT')
-    <div class="mb-2"><input type="text" name="title" class="form-control" value="{{ $item->title }}"></div>
-    <div class="mb-2"><textarea name="description" class="form-control">{{ $item->description }}</textarea></div>
-    <div class="mb-2"><input type="number" name="price" step="0.01" class="form-control" value="{{ $item->price }}"></div>
+<form method="POST" action="{{ route('items.update', $item->id) }}" enctype="multipart/form-data">
+    @csrf
+    @method('PUT')
+
+    <div class="mb-2">
+        <input type="text" name="title" class="form-control" value="{{ old('title', $item->title) }}" required>
+    </div>
+
+    <div class="mb-2">
+        <textarea name="description" class="form-control">{{ old('description', $item->description) }}</textarea>
+    </div>
+
+    <div class="mb-2">
+        <input type="number" name="price" step="0.01" class="form-control" value="{{ old('price', $item->price) }}">
+    </div>
+
     <div class="mb-2">
         <select name="status" class="form-control">
             @foreach(['Available', 'Reserved', 'Sold'] as $status)
@@ -15,6 +26,24 @@
             @endforeach
         </select>
     </div>
+
+    <div class="mb-2">
+        <label>Upload New Images (optional)</label>
+        <input type="file" name="images[]" multiple class="form-control">
+    </div>
+
+    @if($item->images->count())
+        <div class="mb-2">
+            <label>Current Images:</label>
+            <div class="d-flex flex-wrap">
+                @foreach($item->images as $image)
+                    <img src="{{ asset('storage/' . $image->image_url) }}" alt="Image" 
+                         class="img-thumbnail me-2 mb-2" style="width: 120px;">
+                @endforeach
+            </div>
+        </div>
+    @endif
+
     <button class="btn btn-primary">Update</button>
 </form>
 @endsection
