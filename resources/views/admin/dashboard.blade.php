936b8dd10f5d7b71c3322cec513edc00d4a7ad68
Rachel S. Delos Reyes <159454163+dlsrys@users.noreply.github.com>
2025-06-07 16:05:17 +0800

    admin db done

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,6 +2,57 @@
 
 @section('title', 'Admin Dashboard')
 
+<style>
+    .logo2 {
+            display: flex;
+            align-items: center;
+        }
+    .div1 {
+        margin: 100px 0 0 200px;
+    }
+
+    .logo-circle2 {
+        width: 240px;
+        height: 240px;
+        background-color: #e6f49c;
+        border-radius: 50%;
+        margin-right: -80px;
+        margin-right: -80px;
+        padding-left: 10px;
+    }
+
+    .a {
+        margin-left: 100px;
+    }
+
+    .logo-text2 {
+        display: flex;
+        align-items: baseline;
+        font-size: 6.8em;
+    }
+
+    .logo-text2 .bold {
+        font-family: 'Shrikhand', cursive;
+        font-weight: normal;
+    }
+
+    .logo-text2 .script {
+        font-family: 'Great Vibes', cursive;
+        font-size: 1em;
+        margin-left: 6px;
+    }
+</style>
+
 @section('content')
-    <p>Hey anyeong :></p>
+    <div class="div1">
+        <div class="logo2">
+            <div class="logo-circle2"></div>
+            <div class="logo-text2">
+                <span class="bold">Student</span><span class="script">Sells</span>
+            </div>
+            
+        </div>
+            <h2 class="a">a user-friendly platform that enables students to buy and sell</h2>
+    </div> 
+
 @endsection
